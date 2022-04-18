36e09b73b79052bd89be3d6145e8d5c95e8a1b2b
unknown <tahirahfb@gmail.com>
2022-04-18 14:37:25 -0400

    Ex 1.7

--- part1/unicafe/src/components/Results.jsx
+++ part1/unicafe/src/components/Results.jsx
@@ -4,6 +4,9 @@ const Results = (props) => {
             <p>Good: {props.good} </p>
             <p>Neutral: {props.neutral} </p>
             <p>Bad: {props.bad} </p>
+            <p>Total Feedback: {props.good + props.neutral + props.bad} </p>
+            <p>Average: {((props.good - props.bad) / (props.good + props.neutral + props.bad)).toFixed(3)}</p>
+            <p>Positive: {(((props.good) / (props.good + props.neutral + props.bad)) * 100).toFixed(2)}% </p>
         </div>
     )
 }
